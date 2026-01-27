6beb73db9311628e472f599e3373ff7464b47296
fronx <fronx@wurmus.de>
2026-01-28 00:16:28 +0100

    Rename some things for readability

--- spm12/toolbox/DEM/DEM_morphogenesis.m
+++ spm12/toolbox/DEM/DEM_morphogenesis.m
@@ -43,58 +43,60 @@ M(1).E.s  = 1;                             % smoothness
 %--------------------------------------------------------------------------
 L     = 2;
 if L == 2
-    T =[0 0 2 0 0 0 0 0 0 0 0;
-        0 0 0 0 1 0 0 0 0 0 0;
-        2 0 0 0 0 0 4 0 4 0 3;
-        0 0 0 0 1 0 0 0 0 0 0;
-        0 0 2 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 0 0 0 0];
+    Target =[0 0 2 0 0 0 0 0 0 0 0;
+             0 0 0 0 1 0 0 0 0 0 0;
+             2 0 0 0 0 0 4 0 4 0 3;
+             0 0 0 0 1 0 0 0 0 0 0;
+             0 0 2 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0];
 end
  
 if L == 4
-    T =[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 2 0 0 0 3 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 2 0 0 0 0 0 3 0 0 0 4 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 4 0 4 0 1 0 0;
-        0 0 0 2 0 0 0 0 0 3 0 0 0 4 0 0 0 0 0 0 0 1;
-        0 0 0 0 0 0 0 2 0 0 0 3 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+    Target =[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 2 0 0 0 3 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 2 0 0 0 0 0 3 0 0 0 4 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 4 0 4 0 1 0 0;
+             0 0 0 2 0 0 0 0 0 3 0 0 0 4 0 0 0 0 0 0 0 1;
+             0 0 0 0 0 0 0 2 0 0 0 3 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 end
  
-     
-p(:,:,1) = T > 0;
-p(:,:,2) = T == 2 | T == 1;
-p(:,:,3) = T == 3 | T == 1;
-p(:,:,4) = T == 4;
- 
-[y,x] = find(p(:,:,1));
+                                      % fronx:
+p(:,:,1) = Target > 0;                %   any cell (not used for differentiation)
+p(:,:,2) = Target == 2 | Target == 1; %   red color channel
+p(:,:,3) = Target == 3 | Target == 1; %   green color channel
+p(:,:,4) = Target == 4;               %   blue color channel
 
-% fronx: this subtracts the mean
-P.x   = spm_detrend([x(:) y(:)])'/2; 
+[y,x] = find(p(:,:,1));     % cell positions
+
+% fronx: spm_detrend subtracts the mean from the position coordinates
+%        the apostrophe transposes the matrix to have 2 rows (x and y) and n columns (cells)
+%        dividing by 2 scales the coordinates to fit better within the plotting area
+P.position = spm_detrend([x(:) y(:)])'/2;
  
 % signalling of each cell type
 %--------------------------------------------------------------------------
-n     = size(P.x,2);                      % number of cells
+n     = size(P.position,2);                      % number of cells
 m     = size(p,3);                        % number of signals
 j     = find(p(:,:,1));
 for i = 1:m
-    s        = p(:,:,i);
-    P.s(i,:) = s(j);
+    signal        = p(:,:,i);
+    P.signal(i,:) = signal(j);
 end
 
-P.s   = double(P.s); % fronx: signal matrix
-P.c   = morphogenesis(P.x,P.s);           % signal sensed at each position
+P.signal = double(P.signal);
+P.sense   = morphogenesis(P.position, P.signal); % signal sensed at each position
  
 % initialise action and expectations
 %--------------------------------------------------------------------------
 v     = randn(n,n)/8;                     % states (identity)
 g     = Mg([],v,P);
-a.x   = g.x;                              % action (chemotaxis)
-a.s   = g.s;                              % action (signal release)
+action.position = g.position;                  % action (chemotaxis)
+action.signal   = g.signal;                    % action (signal release)
  
  
 % generative process 
@@ -104,17 +106,17 @@ R     = spm_cat({kron(eye(n,n),ones(2,2)) []; [] kron(eye(n,n),ones(4,4));
 
 % level 1 of generative process
 %--------------------------------------------------------------------------
-G(1).g  = @(x,v,a,P) Gg(x,v,a,P);
-G(1).v  = Gg([],[],a,a);
+G(1).g  = @(position, v, action,P) Gg(position, v, action, P);
+G(1).v  = Gg([], [], action, action);
 G(1).V  = exp(16);                         % precision (noise)
 G(1).U  = exp(2);                          % precision (action)
 G(1).R  = R;                               % restriction matrix
-G(1).pE = a;                               % form (action)
+G(1).pE = action;                          % form (action)
  
  
 % level 2; causes (action)
 %--------------------------------------------------------------------------
-G(2).a  = spm_vec(a);                      % endogenous cause (action)
+G(2).a  = spm_vec(action);                  % endogenous cause (action)
 G(2).v  = 0;                               % exogenous  cause
 G(2).V  = exp(16);
  
@@ -124,7 +126,7 @@ G(2).V  = exp(16);
  
 % level 1 of the generative model: 
 %--------------------------------------------------------------------------
-M(1).g  = @(x,v,P) Mg([],v,P);
+M(1).g  = @(position, v, P) Mg([], v, P);
 M(1).v  = g;
 M(1).V  = exp(3);
 M(1).pE = P;
@@ -175,11 +177,11 @@ if SPLIT
     a    = spm_unvec(DEM.qU.a{2}(:,t),DEM.G(1).pE);
     
     v    = v(:,j);
-    g.x  = g.x(:,j);
-    g.s  = g.s(:,j);
-    g.c  = g.c(:,j);
-    a.x  = a.x(:,j) + randn(size(a.x))/512;
-    a.s  = a.s(:,j) + randn(size(a.s))/512;
+    g.position  = g.position(:,j);
+    g.signal    = g.signal(:,j);
+    g.sense  = g.sense(:,j);
+    action.position = action.position(:,j) + randn(size(action.position))/512;
+    action.signal   = action.signal(:,j) + randn(size(action.signal))/512;
     
     DEM.M(1).v = g;
     DEM.M(2).v = v;
@@ -201,13 +203,13 @@ end
 % expected signal concentrations
 %--------------------------------------------------------------------------
 subplot(2,2,2); cla
-A     = max(abs(P.x(:)))*3/2;
+A     = max(abs(P.position(:)))*3/2;
 h     = 2/3;
  
 x     = linspace(-A,A,32);
 [x,y] = ndgrid(x,x);
-x     = spm_detrend([x(:) y(:)])';
-c     = morphogenesis(P.x,P.s,x);
+position     = spm_detrend([x(:) y(:)])';
+c     = morphogenesis(P.position, P.signal, position);
 c     = c - min(c(:));
 c     = c/max(c(:));
 for i = 1:size(c,2)
@@ -254,9 +256,9 @@ spm_figure('GetWin','Figure 2'); clf
 subplot(2,2,1); cla
 for i = 1:m
     for j = 1:n
-        x = P.x(2,j);
-        y = P.x(1,j) + i/6;
-        if P.s(i,j)
+        x = P.position(2,j);
+        y = P.position(1,j) + i/6;
+        if P.signal(i,j)
             plot(x,y,'.','markersize',24,'color','k'); hold on
         else
             plot(x,y,'.','markersize',24,'color','c'); hold on
@@ -270,11 +272,11 @@ hold off
  
 subplot(2,2,2); cla
 for i = 1:n
-    x = P.x(:,i);
-    c = P.s(end - 2:end,i);
-    c = full(max(min(c,1),0));
-    plot(x(2),x(1),'.','markersize',16,'color',c);   hold on
-    plot(x(2),x(1),'h','markersize',12,'color',h*c); hold on
+    position = P.position(:,i);
+    sense = P.signal(end - 2:end,i);
+    sense = full(max(min(sense,1),0));
+    plot(position(2),position(1),'.','markersize',16,'color',sense);   hold on
+    plot(position(2),position(1),'h','markersize',12,'color',h*sense); hold on
 end
  
 title('morphogenesis','Fontsize',16)
@@ -292,10 +294,10 @@ subplot(2,2,3); cla;
 for t = 1:N
     v = spm_unvec(DEM.qU.a{2}(:,t),a);
     for i = 1:n
-        x = v.x(1,i);
-        c = v.s(end - 2:end,i);
-        c = full(max(min(c,1),0));
-        plot(t,x,'.','markersize',16,'color',c); hold on
+        position = v.x(1,i);
+        sense = v.s(end - 2:end,i);
+        sense = full(max(min(sense,1),0));
+        plot(t,position,'.','markersize',16,'color',sense); hold on
     end
 end
  
@@ -314,16 +316,16 @@ for t = 1:N
     v = spm_unvec(DEM.qU.a{2}(:,t),a);
     
     for i = 1:n
-        x = v.x(:,i);
-        c = v.s(end - 2:end,i);
-        c = max(min(c,1),0);
-        plot(x(2),x(1),'.','markersize',8,'color',full(c)); hold on
+        position = v.x(:,i);
+        sense = v.s(end - 2:end,i);
+        sense = max(min(sense,1),0);
+        plot(position(2),position(1),'.','markersize',8,'color',full(sense)); hold on
         
         % destination
         %------------------------------------------------------------------
         if t == N
-            plot(x(2),x(1),'.','markersize',16,'color',full(c));   hold on
-            plot(x(2),x(1),'h','markersize',12,'color',full(h*c)); hold on
+            plot(position(2),position(1),'.','markersize',16,'color',full(c));   hold on
+            plot(position(2),position(1),'h','markersize',12,'color',full(h*c)); hold on
         end
     end
     set(gca,'Color','k');
@@ -349,7 +351,7 @@ if ~exist(movieDir, 'dir')
     mkdir(movieDir);
 end
 for i = 1:length(Mov)
-    imwrite(Mov(i).cdata, fullfile(movieDir, sprintf('frame_%03d.png', i)));
+    imwrite(Mov(i).sensedata, fullfile(movieDir, sprintf('frame_%03d.png', i)));
 end
 fprintf('Frames saved to %s/ (use ffmpeg to create video)\n', movieDir);
 
@@ -361,30 +363,30 @@ return
  
 % sensed signal
 %--------------------------------------------------------------------------
-function c = morphogenesis(x,s,y)
-% x - location of cells
+function sense = morphogenesis(position, signal, y)
+% x - position of cells
 % s - signals released
 % y - location of sampling [default: x]
 %__________________________________________________________________________
  
 % preliminaries
 %--------------------------------------------------------------------------
-if nargin < 3; y = x; end                  % sample locations
-n     = size(y,2);                         % number of locations
-m     = size(s,1);                         % number of signals
-k     = 1;                                 % signal decay over space 
-c     = zeros(m,n);                        % signal sensed at each location
+if nargin < 3; y = position; end           % sample positions
+n     = size(y, 2);                        % number of locations
+m     = size(signal, 1);                   % number of signals
+decay = 1;                                 % signal decay over space 
+sense = zeros(m,n);                        % signal sensed at each location
 for i = 1:n
-    for j = 1:size(x,2)
+    for j = 1:size(position, 2)
         
         % distance
         %------------------------------------------------------------------
-        d      = y(:,i) - x(:,j);
-        d      = sqrt(d'*d);
+        distance = y(:,i) - position(:,j);
+        distance = sqrt(distance'*distance);
         
         % signal concentration
         %------------------------------------------------------------------
-        c(:,i) = c(:,i) + exp(-k*d).*s(:,j);
+        sense(:,i) = sense(:,i) + exp(-decay*distance).*signal(:,j);
  
     end
 end
@@ -392,33 +394,33 @@ end
  
 % first level process: generating input
 %--------------------------------------------------------------------------
-function g = Gg(x,v,a,P)
+function g = Gg(_position, v, action, P)
 global t
 if isempty(t);
-    s = 0;
+    signal = 0;
 else
-    s = (1 - exp(-t*2));
+    signal = (1 - exp(-t*2));
 end
-a        = spm_unvec(a,P);
+action = spm_unvec(action, P);
 
-g.x(1,:) = a.x(1,:);                     % position  signal
-g.x(2,:) = a.x(2,:);                     % position  signal
-g.s      = a.s;                          % intrinsic signal
-g.c      = s*morphogenesis(a.x,a.s);     % extrinsic signal
- 
+g.position(1,:) = action.position(1,:);                   % position  signal
+g.position(2,:) = action.position(2,:);                   % position  signal
+g.signal = action.signal;                                 % intrinsic signal
+g.sense      = signal * morphogenesis(action.position, action.signal);  % extrinsic signal
+
+
 % first level model: mapping hidden causes to sensations
 %--------------------------------------------------------------------------
-function g = Mg(x,v,P)
+function g = Mg(_position, v, P)
 global t
 if isempty(t);
-    s = 0;
+    signal = 0;
 else
-    s = (1 - exp(-t*2));
+    signal = (1 - exp(-t*2));
 end
 
-p    = spm_softmax(v);                   % expected identity
+p = spm_softmax(v);                   % expected identity
 
-g.x  = P.x*p;                            % position
-g.s  = P.s*p;                            % intrinsic signal
-g.c  = s*P.c*p;                          % extrinsic signal
-
+g.position = P.position*p;            % position
+g.signal   = P.signal*p;              % intrinsic signal
+g.sense    = signal*P.sense*p;            % extrinsic signal
